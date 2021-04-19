c1088e8df93c531ac29f1fef805e1c9facc8864c
Gabriel Stellini <gabriel.stellini@fusonic.net>
2021-04-19 23:18:10 +0200

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { EnvironmentModel } from './environment.model';
 
 export const environment: EnvironmentModel = {
   production: true,
-  wsEndpoint: 'ws://192.168.2.199:8085',
+  wsEndpoint: 'ws://127.0.0.1:8085',
   reconnectInterval: 2000
 };
